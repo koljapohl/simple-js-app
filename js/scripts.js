1cc57546fcc0309e226016bd0c09a029ec220e24
Kolja Pohl <kolja.pohl@pohlcon.com>
2020-11-27 14:42:19 +0100

    added Pokémon data

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,26 @@
-alert('Hello world');
-let favoriteFood = "asian styled dishes";
-document.write(favoriteFood);
+let pokemonList = [];
+
+pokemonList = [
+    {
+        name: 'Charmander',
+        height: 6,
+        types: ['fire']
+    },
+    {
+        name: 'Squirtle',
+        height: 5,
+        types: ['water']
+    },
+    {
+        name: 'Togepi',
+        height: 3,
+        types: ['fairy']
+    },
+    {
+        name: 'Bulbasaur',
+        height: 7,
+        types: ['grass', 'poison']
+    }
+];
+
+document.write(pokemonList[0].types);
