84f16912b1ee00ad035986aa8698a5a7bb7a55b3
Kolja Pohl <kolja.pohl@pohlcon.com>
2020-11-30 17:24:10 +0100

    added loop and conditional marking of array elements.

--- js/scripts.js
+++ js/scripts.js
@@ -23,4 +23,19 @@ pokemonList = [
     }
 ];
 
-document.write(pokemonList[0].types);
+let threshHeight = 7;
+let bigText = ' - Wow, that\'s big!';
+for(let i =0; i<pokemonList.length; i++){
+    /*looking at one array element at a time one after another
+    declare a string variable 'text' and assign the pokemon's name as well as its height to it.*/
+    let text = '<p class="pokemon">'+pokemonList[i].name
+    + ' (height: ' + pokemonList[i].height+ ')';
+    /*check if the current pokemon is bigger than the predefined threshold height.
+    If so, add a specific string to the text variable.*/
+    if(pokemonList[i].height>=threshHeight){
+        text = text + bigText + '</p>';
+    } else{
+        text = text + '</p>';
+    }
+    document.write(text);
+}
