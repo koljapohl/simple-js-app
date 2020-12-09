a7e1b95261fa29232e39ca02ad936a8833865b21
Kolja Pohl <kolja.pohl@pohlcon.com>
2020-12-09 10:01:29 +0100

    added modal pattern, rearranged code into IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -86,6 +86,126 @@ let pokemonRepository = (function () {
         });
     }
 
+    //adding form validation UI pattern
+    (function() {
+        let form = document.querySelector('#register-form');
+        let emailInput = document.querySelector('#email');
+        let passwordInput = document.querySelector('#password');
+
+        function validateEmail() {
+            let value = emailInput.value;
+
+            if (!value) {
+                showErrorMessage(emailInput, 'Email is a required field.');
+                return false;
+            }
+            if (value.indexOf('@') === -1) {
+                showErrorMessage(emailInput, 'You must enter a valid email address.');
+                return false;
+            }
+            showErrorMessage(emailInput, null);
+            return true;
+        }
+
+        function validatePassword() {
+            let value = passwordInput.value;
+
+            if (!value) {
+                showErrorMessage(passwordInput, 'Passord is a required field.');
+                return false;
+            }
+            if(value.length < 8) {
+                showErrorMessage(passwordInput, 'The password needs to be at least 8 characters long.');
+                return false;
+            }
+            showErrorMessage(passwordInput, null);
+            return true;
+        }
+
+        function validateForm() {
+            let isValidEmail = validateEmail();
+            let isValidPassword = validatePassword();
+            return isValidEmail && isValidPassword;
+        }
+
+        function showErrorMessage(input, message) {
+            let container = input.parentElement; //that's the .input-wrapper
+
+            //Remove possible existing error
+            let error = container.querySelector('.error-message');
+            if (error) {
+                container.removeChild(error);
+            }
+
+            //Add error message if the message itself is not empty
+            if (message) {
+                let error = document.createElement('div');
+                error.classList.add('error-message');
+                error.innerText = message;
+                container.appendChild(error);
+            }
+        }
+
+        emailInput.addEventListener('input', validateEmail);
+        passwordInput.addEventListener('input', validatePassword);
+        form.addEventListener('submit', (e) => {
+            e.preventDefault(); //Do not submit to the server
+
+            if(validateForm()) {
+                alert('Success!');
+            }
+        })
+    })();
+
+    //Modal event
+    function showModal(title, text) {
+        let modalContainer = document.querySelector('#modal-container');
+        //clear all existing content
+        modalContainer.innerHTML = '';
+        let modal = document.createElement('div');
+        modal.classList.add('modal');
+        //Add modal content
+        let closeButtonElement = document.createElement('button');
+        closeButtonElement.classList.add('modal-close');
+        closeButtonElement.innerText = 'Close';
+        closeButtonElement.addEventListener('click', hideModal);
+
+        let titleElement = document.createElement('h1');
+        titleElement.innerText = title;
+
+        let contentElement = document.createElement('p');
+        contentElement.innerText = text;
+
+        modal.appendChild(closeButtonElement);
+        modal.appendChild(titleElement);
+        modal.appendChild(contentElement);
+        modalContainer.appendChild(modal);
+
+        modalContainer.classList.add('is-visible');
+    }
+
+    document.querySelector('#show-modal').addEventListener('click', (e) => {
+        showModal('Modal title', 'This ist the modal content!');
+    });
+
+    function hideModal() {
+        let modalContainer = document.querySelector('#modal-container');
+        modalContainer.classList.remove('is-visible');
+    }
+
+    window.addEventListener('keydown', (e) => {
+        let modalContainer = document.querySelector('#modal-container');
+        if (e.key === 'Escape' && modalContainer.classList.contains('is-visible')) {
+            hideModal();
+        }
+    });
+    document.querySelector('#modal-container').addEventListener('click', (e) => {
+        let target = e.target;
+        if (target === document.querySelector('#modal-container')) {
+            hideModal();
+        }
+    });
+
     return {
         getAll: getAll,
         addv: addv,
@@ -104,74 +224,3 @@ pokemonRepository.loadList().then(function() {
         pokemonRepository.addListItem(pokemon);
     });
 });
-
-//adding form validation UI pattern
-(function() {
-    let form = document.querySelector('#register-form');
-    let emailInput = document.querySelector('#email');
-    let passwordInput = document.querySelector('#password');
-
-    function validateEmail() {
-        let value = emailInput.value;
-
-        if (!value) {
-            showErrorMessage(emailInput, 'Email is a required field.');
-            return false;
-        }
-        if (value.indexOf('@') === -1) {
-            showErrorMessage(emailInput, 'You must enter a valid email address.');
-            return false;
-        }
-        showErrorMessage(emailInput, null);
-        return true;
-    }
-
-    function validatePassword() {
-        let value = passwordInput.value;
-
-        if (!value) {
-            showErrorMessage(passwordInput, 'Passord is a required field.');
-            return false;
-        }
-        if(value.length < 8) {
-            showErrorMessage(passwordInput, 'The password needs to be at least 8 characters long.');
-            return false;
-        }
-        showErrorMessage(passwordInput, null);
-        return true;
-    }
-
-    function validateForm() {
-        let isValidEmail = validateEmail();
-        let isValidPassword = validatePassword();
-        return isValidEmail && isValidPassword;
-    }
-
-    function showErrorMessage(input, message) {
-        let container = input.parentElement; //that's the .input-wrapper
-
-        //Remove possible existing error
-        let error = container.querySelector('.error-message');
-        if (error) {
-            container.removeChild(error);
-        }
-
-        //Add error message if the message itself is not empty
-        if (message) {
-            let error = document.createElement('div');
-            error.classList.add('error-message');
-            error.innerText = message;
-            container.appendChild(error);
-        }
-    }
-
-    emailInput.addEventListener('input', validateEmail);
-    passwordInput.addEventListener('input', validatePassword);
-    form.addEventListener('submit', (e) => {
-        e.preventDefault(); //Do not submit to the server
-
-        if(validateForm()) {
-            alert('Success!');
-        }
-    })
-})();
